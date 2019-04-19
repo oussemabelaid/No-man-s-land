9bb7e29ad19384ed9269676cc7cbdb6644238650
AnisMoalla <47814013+AnisMoalla@users.noreply.github.com>
2019-04-19 05:37:50 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,54 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
+#include <stdlib.h>
+#include "SDL/SDL.h"
+#include "objet .h"
 #include <SDL/SDL_image.h>
-#include "file.h" 
 
-int bounding_box(SDL_Rect Personnage,SDL_Rect Ennemi)
+int main(void){
+
+SDL_Surface *screen;
+SDL_Surface *image;
+
+objet o1;
+objet o2;
+char pause;
+SDL_Rect positionscreen;
+SDL_Event event;
+SDL_Init(SDL_INIT_VIDEO);
+screen=SDL_SetVideoMode(800,800,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+
+
+image =IMG_Load("bg.png");
+
+positionscreen.x=0;
+positionscreen.y=0;
+
+init_objet (&o1,0,100,"o1.png");
+init_objet (&o2,320,100,"o2.png");
+
+int done =1 ;
+while (done)
 {
-if((Personnage.x<Ennemi.x)&&((Personnage.x+Personnage.w)>=Ennemi.x)&&(Personnage.y<=(Ennemi.y+Ennemi.h))&&((Personnage.y+Personnage.h)>=Ennemi.y))
-return 2;
-if(((Personnage.x+Personnage.w)>=Ennemi.x)&&(Personnage.x<=(Ennemi.x+Ennemi.w))&&(Personnage.y>Ennemi.y)&&(Personnage.y<=(Ennemi.y+Ennemi.h)))
-return 1;
-if((Personnage.x>Ennemi.x)&&(Personnage.x<=(Ennemi.x+Ennemi.w))&&(Personnage.y<=(Ennemi.y+Ennemi.h))&&((Personnage.y+Personnage.h)>=Ennemi.y))
-return 4;
-if((Personnage.y<Ennemi.y)&&((Personnage.y+Personnage.h)>=Ennemi.y)&&((Personnage.x+Personnage.w)>=Ennemi.x)&&(Personnage.x<=(Ennemi.x+Ennemi.w)))
-return 3;
-else 
+SDL_BlitSurface(image,NULL,screen,&positionscreen);
+afficher_objet(o1,screen);
+afficher_objet(o2,screen);
+while(SDL_PollEvent(&event))
+{
+switch(event.type)
+{
+case SDL_QUIT:
+done= 0;
+break;
+}
+SDL_Flip(screen);
+}
+SDL_Flip(screen);
+}
+SDL_FreeSurface(image);
+
+
+DL_Quit();
+return EXIT_SUCCESS;
+pause= getchar();
 return 0;}
+
